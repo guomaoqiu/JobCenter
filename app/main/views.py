6a4037d0c9845615ec4b67f7c0fecc2b0037b3f9
guomaoqiu <guomaoqiu@sina.com>
2019-03-21 18:27:23 +0800

    🔞Initial commit

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@
 # @File Name: views.py
 # @Date:   2019-03-13 10:07:12
 # @Last Modified by:   guomaoqiu
-# @Last Modified time: 2019-03-21 18:25:50
+# @Last Modified time: 2019-03-21 18:27:11
 
 from flask import render_template, abort, request,jsonify, redirect,url_for,flash, current_app, send_from_directory
 from . import main
@@ -17,7 +17,7 @@ from .forms import JobDateForm,JobCronForm,JobIntervalForm
 import json as simplejson
 import requests, subprocess, json, datetime
 from datetime import date
-from app.job.views import show_jobs,job_logz
+from app.job.views import show_jobs,job_log
 from ..models import TaskLog
 from .. import scheduler
 from app.job.core import jobfromparm
