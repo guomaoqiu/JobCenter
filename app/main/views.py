7df18b929a2139ce41642288cc011d57dc64e747
guomaoqiu <guomaoqiu@sina.com>
2019-03-21 16:52:01 +0800

    🔞Initial commit

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@
 # @File Name: views.py
 # @Date:   2019-03-13 10:07:12
 # @Last Modified by:   guomaoqiu
-# @Last Modified time: 2019-03-21 14:49:54
+# @Last Modified time: 2019-03-21 16:51:53
 
 from flask import render_template, abort, request,jsonify, redirect,url_for,flash, current_app, send_from_directory
 from . import main
@@ -123,3 +123,9 @@ def createjob():
             flash('定时任务 {0} 添加失败 {1}'.format(data['id'],e),'danger')
     flash("Demo环境已关闭任务添加功能","warning")        
     return render_template('create_job.html',form_date=form_date,form_cron=form_cron,form_interval=form_interval)
+
+@main.route('/stdout/<id>')
+def stdout(id):
+    result = db.session.query(TaskLog).filter_by(id=id).first()
+    stdout = result.to_json()['stdout']
+    return render_template('stdout.html',stdout=stdout)
