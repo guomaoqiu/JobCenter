da9025c3e71ae62503ec454270ac322071412de9
guomaoqiu <guomaoqiu@sina.com>
2019-05-22 17:45:45 +0800

    🔞Initial commit

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@
 # @File Name: views.py
 # @Date:   2019-03-13 10:07:12
 # @Last Modified by:   guomaoqiu
-# @Last Modified time: 2019-05-22 17:30:35
+# @Last Modified time: 2019-05-22 17:45:42
 
 from flask import render_template, abort, request,jsonify, redirect,url_for,flash, current_app, send_from_directory
 from . import main
@@ -23,7 +23,7 @@ from .. import scheduler
 from app.job.core import jobfromparm
 
 #demo环境切换
-DEMO_ENV=True
+DEMO_ENV=False
 
 @main.route('/')
 # @login_required
