67d3f981b31f2e297cc983dfb2a155cc73a8f742
guomaoqiu <guomaoqiu@sina.com>
2020-06-23 01:03:05 +0800

    Initial commit

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@
 # @File Name: views.py
 # @Date:   2019-03-13 10:07:12
 # @Last Modified by:   guomaoqiu
-# @Last Modified time: 2020-06-23 01:00:27
+# @Last Modified time: 2020-06-23 01:02:42
 
 from flask import render_template, abort, request,jsonify, redirect,url_for,flash, current_app, send_from_directory
 from . import main
@@ -36,7 +36,6 @@ def dingding():
             "agent": str(request.user_agent),
             "title": "***[{0}支付通知***[MSG]".format( pay_method)
         }
-        dingding(dingding_send_info)
         try:
             dingding(dingding_send_info)
             return jsonify({"result":True, "message":"OK, 您的付款信息已經通知客服小姐姐啦~.\n30分鐘內為您開通VIP~"})
